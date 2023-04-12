40b951522e2015c341581a9c21e24a15f8e57450
Amir Nejat <amirnejat310@gmail.com>
2023-04-12 10:18:36 +0330

    made the components folder/Signup  file and made the half of my input

---
+++ src/components/SignUp.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+
+const SignUp = () => {
+
+    const[data,setData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+        isAccepted: false,
+    })
+
+    const changeHandler = event => {
+        if (event.target.name === 'isAccepted') {
+            setData({...data, [event.target.name]: event.target.checked})
+        }else {
+            setData({...data, [event.target.name]: event.target.value})
+        }
+        console.log(data)
+    }
+
+    return (
+        <div>
+            <form>
+                <h2>SignUp</h2>
+                <div>
+                    <label>name</label>
+                    <input type="text" name='name' value={data.name} onChange={changeHandler}/>
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input type="email" name='email' value={data.email}  onChange={changeHandler}/>
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="password" name='password' value={data.password}  onChange={changeHandler}/>
+                </div>
+                <div>
+                    <label>confirmPassword</label>
+                    <input type="password" name='confirmPassword' value={data.confirmPassword}  onChange={changeHandler}/>
+                </div>
+                <div>
+                    <label>i accept terms of privacy policy</label>
+                    <input type="checkbox" name='isAccepted' value={data.isAccepted}  onChange={changeHandler}/>
+                </div>
+                <div>
+                    <a href='http'>Login</a>
+                    <button type='submit'>SignUp</button>
+
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default SignUp;
